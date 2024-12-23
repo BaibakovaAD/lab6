164d004b6736083999dae9daa1b27ce2ff402b2f
BaibakovaAD <nastya.bay.ry@gmail.com>
2024-12-23 20:29:08 +0500

    Update AnotInvoke.java

--- lab6/src/AnotInvoke.java
+++ lab6/src/AnotInvoke.java
@@ -1,6 +1,6 @@
 public class AnotInvoke {
     @Invoke
     public void start() {
-        System.out.println("Этот класс аннотирован Invoke!");
+        System.out.println("Invoke используется для маркировки методов, которые должны быть вызваны.");
     }
 }
