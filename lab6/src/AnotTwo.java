48fb15d82ed04ec3224c9de3e0ae9b004ac73aa8
BaibakovaAD <nastya.bay.ry@gmail.com>
2024-12-23 20:30:33 +0500

    Update AnotTwo.java

--- lab6/src/AnotTwo.java
+++ lab6/src/AnotTwo.java
@@ -1,6 +1,6 @@
 @Two(first = "str", second = 100)
 public class AnotTwo {
     public void start() {
-        System.out.println("Этот класс аннотирован @Two!");
+        System.out.println("Этот класс аннотирован @Two и содержит два обязательных свойства: строку и число");
     }
 }
