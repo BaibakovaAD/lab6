5214b9541d4f67ad5131e88b66bcbc04f1c912a2
BaibakovaAD <nastya.bay.ry@gmail.com>
2024-12-23 20:31:17 +0500

    Update AnotValidate.java

--- lab6/src/AnotValidate.java
+++ lab6/src/AnotValidate.java
@@ -1,6 +1,6 @@
 @Validate(value = {String.class, Integer.class})
 public class AnotValidate {
     public void start() {
-        System.out.println("Этот класс аннотировано @Validate!");
+        System.out.println("Этот класс аннотировано @Validate. \n@Validate используется для проверки, поддерживает ли класс указанные типы.");
     }
 }
