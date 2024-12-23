1e708e245f674a63bcc02aa71f5b147f9240e613
BaibakovaAD <nastya.bay.ry@gmail.com>
2024-12-23 20:36:03 +0500

    Update Default.java

--- lab6/src/Default.java
+++ lab6/src/Default.java
@@ -3,5 +3,5 @@ import java.lang.annotation.*;
 @Target({ElementType.TYPE, ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Default {
-    Class<?> value(); // обязательное тк иниц-но. Тип - любой, тк "?"
+    Class<?> value();
 }
