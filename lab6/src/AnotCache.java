1bbc4f9c725d919be574f39ef15ceae15a82feef
BaibakovaAD <nastya.bay.ry@gmail.com>
2024-12-23 20:28:05 +0500

    Update AnotCache.java

--- lab6/src/AnotCache.java
+++ lab6/src/AnotCache.java
@@ -1,6 +1,6 @@
 @Cache(value = {"item1", "item2", "item3"})
 public class AnotCache {
     public void start() {
-        System.out.println("Этот класс аннотирован @Cache!");
+        System.out.println("@Cache Задает кэшируемые элементы в виде массива строк. \nЭтот класс аннотирован @Cache");
     }
 }
