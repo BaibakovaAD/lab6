e5865837a8c501acac758ae1b2330506aa08e5cf
BaibakovaAD <nastya.bay.ry@gmail.com>
2024-12-24 09:57:04 +0500

    Update Validate.java

--- lab6/src/Validate.java
+++ lab6/src/Validate.java
@@ -3,5 +3,5 @@ import java.lang.annotation.*;
 @Target({ElementType.TYPE, ElementType.ANNOTATION_TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Validate {
-    Class<?>[] value(); // обязательное, тк неинициализированное
+    Class<?>[] value();
 }
